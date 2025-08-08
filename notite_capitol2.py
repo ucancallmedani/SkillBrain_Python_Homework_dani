b72ccc738fc0ea128e34af4f0a474db9fb2221ec
Stefan Statescu <stefan_statescu@yahoo.com>
2025-08-08 15:15:48 +0300

    Capitol 2

---
+++ notite_capitol2.py
@@ -0,0 +1,47 @@
+# Operatori
+
+# 1. Operatori matematici
+hello_world = "Hello" + "World"
+adunare = 7 + 3
+# print(adunare)
+
+# + aduanre
+# - scadere
+# * inmultire
+# / impartirea normala (float)
+# // impartire parte intreaga
+# % impartire cu rest
+
+rest = 7 % 3
+# print("rest", rest)
+
+partea_intreaga = 7 // 3
+# print("Partea intreaga", partea_intreaga)
+
+inmultire = 7 * 3
+# print("Inmultire", inmultire)
+
+lungime = 7
+latime = 5
+
+perimetru_dreptunghi = lungime * latime
+# print(perimetru_dreptunghi)
+
+# 2. Operatori de comparatie
+# a > b
+# a < b
+# a >= b
+# a <= b
+# a == b
+# a != b
+
+a = 7
+b = 3
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+
+varsta  = int(input("Care este varsta dvs? "))
+print("Major?", varsta >= 18)
